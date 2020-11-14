5a82be229a87b129fa4f38f5c1b4bce5810b31b1
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2020-11-14 11:41:54 +0100

    Edycja uzytkownika cz.2

--- PortalRandkowy.SPA/src/app/Users/user-edit/user-edit.component.ts
+++ PortalRandkowy.SPA/src/app/Users/user-edit/user-edit.component.ts
@@ -1,6 +1,10 @@
+import { NgForOf } from '@angular/common';
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -10,13 +14,21 @@ import { User } from 'src/app/_models/user';
 export class UserEditComponent implements OnInit {
 
     user: User;
+  @ViewChild('editForm') editForm: NgForm;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit(): void{
     this.route.data.subscribe(data => {
       this.user = data.user;
     });
   }
+
+  updateUser(): void{
+    console.log(this.user);
+    this.alertify.success('Profil pomyslnie zaktualizowany');
+    this.editForm.reset(this.user);
+  }
+
 
 }
