648fd18e5f1696c7ef02fa7d74d86b663d554a90
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2020-11-19 11:40:21 +0100

    Reavtive Forms, formularz rejestracji reaktywny

--- PortalRandkowy.SPA/src/app/register/register.component.ts
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 
@@ -11,18 +12,26 @@ export class RegisterComponent implements OnInit {
 
   @Output() cancelRegister: EventEmitter<any> = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
   }
 
   register(): void{
-    this.authService.register(this.model).subscribe(() => {
-      this.alertify.success('rejestracja udana');
-    }, error => {
-      this.alertify.error(error);
-    });
+    //this.authService.register(this.model).subscribe(() => {
+   //   this.alertify.success('rejestracja udana');
+   // }, error => {
+   //   this.alertify.error(error);
+   // });
+    console.log(this.registerForm.value);
+
   }
 
   cancel(): void{
