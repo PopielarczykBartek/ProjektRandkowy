a76b067e745fd2bc641f508f3e40fb95a788d3cd
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2020-11-19 11:48:55 +0100

    Prosta walidacja danych

--- PortalRandkowy.SPA/src/app/register/register.component.ts
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 
@@ -18,14 +18,14 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
-      username: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl()
+      username: new FormControl('Podaj nazwe uzytkownika', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
+      confirmPassword: new FormControl('', Validators.required)
     });
   }
 
   register(): void{
-    //this.authService.register(this.model).subscribe(() => {
+   // this.authService.register(this.model).subscribe(() => {
    //   this.alertify.success('rejestracja udana');
    // }, error => {
    //   this.alertify.error(error);
