58f2a864ec277901a8e8ad5211dca7a432139739
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2020-12-03 16:08:20 +0100

    uaktualnie metody rejestracji. Mapper. API

--- ProjektRandkowy.API/Dtos/UserForRegisterDto.cs
+++ ProjektRandkowy.API/Dtos/UserForRegisterDto.cs
@@ -13,6 +13,24 @@ namespace ProjektRandkowy.Dtos
         [Required(ErrorMessage="Haslo jest wymagane")]
         [StringLength(12,MinimumLength=6,ErrorMessage ="Haslo musi sie skladac od 6 do 12 znakow")]
         public string Password { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string ZodiacSign { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
 
     }
 }
