b12c9a14ebbce596e05091f185d468537183371f
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2020-12-11 11:32:18 +0100

    filtrowanie API

--- ProjektRandkowy.API/Controllers/UsersController.cs
+++ ProjektRandkowy.API/Controllers/UsersController.cs
@@ -29,6 +29,16 @@ namespace ProjektRandkowy.Controllers
         [HttpGet]
         public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
         {
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            var userFromRepo = await _repo.GetUser(currentUserId);
+
+            userParams.UserId = currentUserId;
+
+            if (string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = userFromRepo.Gender == "mezczyzna" ? "kobieta" : "mezczyzna";
+            }
+
             var users = await _repo.GetUsers(userParams);
 
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
